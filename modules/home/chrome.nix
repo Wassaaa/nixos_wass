5b217ff74c5d281547bfb850350e52db164fc69d
Allar Klein <allarklein@gmail.com>
2025-10-20 20:55:47 +0300

    disabled most chrome flags that seem to not be needed now, and added a new one that prevents a crash when moving windows around

--- modules/home/chrome.nix
+++ modules/home/chrome.nix
@@ -2,10 +2,11 @@
   programs.google-chrome = {
     enable = true;
     commandLineArgs = [
-      "--wayland-per-window-scaling=1"
-      "--ozone-platform-hint=wayland"
-      "--wayland-ui-scaling=1"
-      "--wayland-text-input-v3=1"
+      "--disable-features=WaylandWpColorManagerV1"
+      # "--wayland-per-window-scaling=1"
+      # "--ozone-platform-hint=wayland"
+      # "--wayland-ui-scaling=1"
+      # "--wayland-text-input-v3=1"
     ];
   };
 }
