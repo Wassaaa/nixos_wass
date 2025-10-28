523b51adf1332e6a2cd1476dbaad5b38b451a0b0
Allar Klein <allarklein@gmail.com>
2025-10-28 18:51:35 +0200

    ne need for those envs to be se tI think

--- modules/core/features/desktop/de-hyprland.nix
+++ modules/core/features/desktop/de-hyprland.nix
@@ -2,12 +2,6 @@
 {
   programs.hyprland.enable = true;
 
-  environment.sessionVariables = {
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-  };
-
   environment.etc."xdg/wayland-sessions/hyprland.desktop".text = ''
     [Desktop Entry]
     Name=Hyprland
