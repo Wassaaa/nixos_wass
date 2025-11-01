0f556bc28405fc9128b7ac1bdc7a23aa13fc198a
Allar Klein <allarklein@gmail.com>
2025-11-01 10:52:50 +0200

    vencord discord

--- hosts/wassaa/host-packages.nix
+++ hosts/wassaa/host-packages.nix
@@ -5,12 +5,15 @@
     audacity
     nodejs
     obs-studio
-    (pkgs.makeDesktopItem {
-      name = "discord";
-      exec = "${pkgs.discord}/bin/discord --use-gl=desktop --enable-gpu-rasterization  --enable-features=UseOzonePlatform --ozone-platform=wayland";
-      desktopName = "Discord";
-      icon = "${pkgs.xfce.xfce4-icon-theme}/share/icons/apps/scalable/discord.svg";
+    # (pkgs.makeDesktopItem {
+    #   name = "discord";
+    #   exec = "${pkgs.discord}/bin/discord";
+    #   desktopName = "Discord";
+    #   icon = "${pkgs.xfce.xfce4-icon-theme}/share/icons/apps/scalable/discord.svg";
+    # })
+    (discord.override {
+      # withOpenASAR = true;
+      withVencord = true;
     })
-    (discord.override { withOpenASAR = true; })
   ];
 }
